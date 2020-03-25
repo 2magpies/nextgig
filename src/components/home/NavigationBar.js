1830f7c49f7c1b2568218bb7d2e0fd69fbc3508a
2magpies <daphnemintz@yahoo.com>
2020-03-25 09:37:09 -0400

    removed contact and about from nav bar

--- src/components/home/NavigationBar.js
+++ src/components/home/NavigationBar.js
@@ -32,12 +32,7 @@ export const NavigationBar = () => (
           <Nav.Item>
             <Nav.Link href="/venues">Venues</Nav.Link>
           </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/about">About</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav.Item>
+          
         </Nav>
       </Navbar.Collapse>
     </Navbar>
